0a29e8c1f689ff8d33f39b894e51dd76d68be00f
Valdemir Lisoa <vl.valdemir7@gmail.com>
2026-07-29 21:11:11 -0300

    Update Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -5,19 +5,20 @@ public class Personagem {
     int pontosDeVida;
     double poderBase;
 
+    //Construtor
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase
 
     }
-
+     
+    // Metodo para exibir o status
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
+        
 
     }
 }
